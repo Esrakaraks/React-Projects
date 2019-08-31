c37f9892f14a180feefb1e4776f7f3a8b2c3b54d
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-01 00:03:13 +0300

    update

--- form/src/form.js
+++ form/src/form.js
@@ -6,27 +6,35 @@ import React from 'react';
  export class Form extends React.Component  {
  	constructor(props){
 		super(props);
-		this.state={name :' ',
+		this.state={ firstname :' ',
 					lastname:' ',
 					email :' ',
 					password :' '
 
 		}
+   }
 
-		
-		
+ 	change=(e)=>{
+ 		this.setState({
+      [e.target.name]: e.target.value
+ 		
 
-		
-	}
+    });
+ 		
 
- 	nameChange=(e)=>{
- 		 this.setState({name: e.target.value});
- 		 console.log(this.state.value);
+ 	}
 
- 		}
- 	handleSubmit =(e) =>{
- 		 alert('A name was submitted: ' + this.state.value);
-   	}
+
+ 	 onSubmit = e => {
+ 	 	  alert('A name was submitted: ' + this.state.firstname +  " " + this.state.lastname + " " +
+ 	 	  this.state.email  + " " + this.state.password );
+ 	 	    
+   		 e.preventDefault();
+
+   			 
+
+
+}
  	
 
 
@@ -38,25 +46,25 @@ import React from 'react';
 
  				   <div className="form-group">
     					<label htmlFor="Name">Name:</label>
-    					<input type="text" className="form-control"  value ={this.state.value} onChange={this.nameChange} id="name" placeholder="Enter Name"/>
+    					<input type="text" className="form-control"  name="firstname"  value ={this.state.firsname} onChange={this.change}  placeholder="Enter Name"/>
    					</div>
 
    					<div className="form-group">
     					<label htmlFor="lastname">LastName:</label>
-    					<input type="text" className="form-control" id="lastname" placeholder="Enter Lastname"/>
+    					<input type="text" className="form-control" name="lastname"  value ={this.state.lastname} onChange={this.change} placeholder="Enter Lastname"/>
    					</div>  
 
   					<div className="form-group">
     					<label htmlFor="exampleInputEmail1">Email address:</label>
-    					<input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
+    					<input type="email" className="form-control" name="email" value ={this.state.email} onChange={this.change} placeholder="Enter email"/>
    					</div>
 
   					<div className="form-group">
     					<label htmlFor="exampleInputPassword1">Password</label>
-    					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+    					<input type="password" className="form-control" name="password" value ={this.state.password} onChange={this.change} placeholder="Password"/>
  					</div>
 
-  					<button type="submit" id="submit" onSubmit={this.handleSubmit} className="btn btn-primary">Submit</button>
+  					<button type="submit" id="submit" onClick={this.onSubmit} className="btn btn-primary">Submit</button>
 				</form>
  				
  			</div>
