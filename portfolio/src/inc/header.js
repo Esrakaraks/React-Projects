2c470d2e0380832c899faf307154e14d135474d3
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-02 11:25:01 +0300

    update

---
+++ portfolio/src/inc/header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Layout,Header,Drawer,Navigation,Content} from 'react-mdl';
+
+export  class Headerr extends React.Component{
+
+	render(){
+
+		return(
+
+			<div className="demo-big-content">
+    			<Layout>
+      			  <Header title="Title" scroll>
+	            		<Navigation>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+	            		</Navigation>
+        			</Header>
+        			<Drawer title="Title">
+            			<Navigation>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+			                <a href="#">Link</a>
+           			 	</Navigation>
+        			</Drawer>
+        			<Content>
+            			<div className="page-content" />
+        			</Content>
+    			</Layout>
+			</div>
+
+			);
+	}
+
+}
