a0bf15ed2f34351584c6694500575b167d4ec9e9
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-04 18:59:13 +0300

    update

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Grid,Cell} from 'react-mdl';
+
 
 export  class LandingPage extends React.Component{
 
 	render(){
 		return(
 			<div className="content-class">
-				<div className="landingpage-class">
+				<div className="content">
 					<h3 className="h3-class">FULL STACK WEB DEVELOPER</h3>
 				</div>
-				<p> HTML | CSS | jAVASCRİPT | NODEJS |REACTJS </p> 
-				
 			</div>
 
 		);
