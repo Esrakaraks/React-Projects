7c52c0bd273e1ac6b6df43e26b58a383ab7fab0e
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-04 19:09:30 +0300

    update

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -6,9 +6,8 @@ export  class LandingPage extends React.Component{
 	render(){
 		return(
 			<div className="content-class">
-				<div className="content">
-					<h3 className="h3-class">FULL STACK WEB DEVELOPER</h3>
-				</div>
+			   <h3 className="h3-class">FULL STACK WEB DEVELOPER</h3>
+				
 			</div>
 
 		);
