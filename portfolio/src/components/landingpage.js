a77da0c4af8768e75ab39b534bcfcd34cdcd857c
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-09-05 13:50:49 +0300

    update

--- portfolio/src/components/landingpage.js
+++ portfolio/src/components/landingpage.js
@@ -6,7 +6,7 @@ export  class LandingPage extends React.Component{
 	render(){
 		return(
 			<div className="content-class">
-			   <h3 className="h3-class">FULL STACK WEB DEVELOPER</h3>
+			  
 				
 			</div>
 
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,13 +1,36 @@
 import React  from 'react';
+import {Tabs ,Tab} from 'react-mdl';
 
 export  class Projects extends React.Component{
 
-	render(){
-		return(
-			<div>Projects Page</div>
+	constructor(props) {
+        super(props)
+        this.state = { activeTab: 2 };
+    }
 
+  
+   toggleTab(){
+   		
 
-		);
-	}
+   }
+    render() {
+        return (
+            <div className="demo-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>NodeJs</Tab>
+					<Tab>ReactJs</Tab>
+                    <Tab>ReactNative</Tab>
+                     <Tab>VueJs</Tab>
+                </Tabs>
+                <section>
+                    <div className="Projects-gird">
+                       {this.toggleTab()}
+                     </div>
+                </section>
+               
+           </div>
+
+        );
+    }
 
 }
