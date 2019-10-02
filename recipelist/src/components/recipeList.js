0dd4cfbe4e218708f33102d24eb8f107ddd69787
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-02 23:44:11 +0300

    update

--- recipelist/src/components/recipeList.js
+++ recipelist/src/components/recipeList.js
@@ -3,10 +3,10 @@ import {Textfield , Grid ,Cell} from 'react-mdl';
 export class RecipeList extends React.Component{
 	render(){
 			return(
-				<div class="container">
-					<h3 class ="h2-title">SEARCH FOR RECİPE WİTH FOOD2FORK</h3>
-					<p class="type"> type Recipes separated by comma</p>
-					<div class = "textfield-search">
+				<div className="container">
+					<h3 className ="h2-title">SEARCH FOR RECİPE WİTH FOOD2FORK</h3>
+					<p className="type"> type Recipes separated by comma</p>
+					<div className = "textfield-search">
 					       <Textfield
 							    onChange={() => {}}
 							    label="Search"
@@ -25,12 +25,13 @@ export class RecipeList extends React.Component{
 					</div>
 					<h2 class="recipe-list"> RECİPE LIST</h2>
 
-				<div style={{width: '80%', margin: 'auto'}}>
+				<div className="grid-style" >
 					<Grid className="demo-grid-1">
-				        <Cell col={4}>4</Cell>
+				        <Cell col={4}><img src ="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/></Cell>
 				        <Cell col={4}>4</Cell>
 				        <Cell col={4}>4</Cell>
 				    </Grid>
+				   
 				</div>
 			</div>
 			);
