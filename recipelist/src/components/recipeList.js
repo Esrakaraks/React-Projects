64189fe7ac8a822c8d40d4748f1200df3305d910
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-02 22:43:58 +0300

    update

--- recipelist/src/components/recipeList.js
+++ recipelist/src/components/recipeList.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import {Textfield } from 'react-mdl';
 export class RecipeList extends React.Component{
 	render(){
 			return(
 				<div class="container">
 					<h2 class ="h2-title">SEARCH FOR RECİPE WİTH FOOD2FORK</h2>
+					<Textfield
+					    onChange={() => {}}
+					    label="Text lines..."
+					    rows={3}
+					    style={{width: '200px'}}
+					/>
+
+					{/* Expandable Textfield */}
+					<Textfield
+					    onChange={() => {}}
+					    label="Expandable Input"
+					    expandable
+					    expandableIcon="search"
+					/>
 
 				</div>
 			);
--- recipelist/src/index.js
+++ recipelist/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
