3170129cf891de716c195033c4c9ec73390b605f
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-02 23:24:44 +0300

    update

--- recipelist/src/components/recipeList.js
+++ recipelist/src/components/recipeList.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Textfield } from 'react-mdl';
+import {Textfield , Grid ,Cell} from 'react-mdl';
 export class RecipeList extends React.Component{
 	render(){
 			return(
 				<div class="container">
-					<h2 class ="h2-title">SEARCH FOR RECİPE WİTH FOOD2FORK</h2>
+					<h3 class ="h2-title">SEARCH FOR RECİPE WİTH FOOD2FORK</h3>
 					<p class="type"> type Recipes separated by comma</p>
 					<div class = "textfield-search">
 					       <Textfield
 							    onChange={() => {}}
-							    label="Text lines..."
+							    label="Search"
 							    rows={3}
 							    style={{width: '55rem'}}
 							    
@@ -23,7 +23,16 @@ export class RecipeList extends React.Component{
 						/>
 
 					</div>
+					<h2 class="recipe-list"> RECİPE LIST</h2>
+
+				<div style={{width: '80%', margin: 'auto'}}>
+					<Grid className="demo-grid-1">
+				        <Cell col={4}>4</Cell>
+				        <Cell col={4}>4</Cell>
+				        <Cell col={4}>4</Cell>
+				    </Grid>
 				</div>
+			</div>
 			);
 
 	}
